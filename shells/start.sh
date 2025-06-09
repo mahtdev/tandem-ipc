a107b8cd91b191910ba783a2c55ca5d5e82a57b5
Miguel Hernandez <mhernandez@kssoluciones.ocm>
2025-06-09 15:39:43 -0600

    modificando la VMO en el shell start.sh

--- shells/start.sh
+++ shells/start.sh
@@ -9,4 +9,4 @@ export JAVA_HOME=/usr/tandem/nssjava/jdk180_180
 export PATH=$PATH:${JAVA_HOME}/bin
 
 add_define=TCP^PROCESS^NAME class=map File=\$ZTC9
-run -cpu=${PROCESS_NUMBER} -name=/G/${PROCESS_NAME} java -ks.process.name=BOX6 -jar test-atalla-1.0.0.0.jar
+run -cpu=${PROCESS_NUMBER} -name=/G/${PROCESS_NAME} java -Dks.process.name=BOX6 -jar test-atalla-1.0.0.0.jar
